e6c8e24cdbfb6af3a108ba30b922a46eb08926d6
sam bacha <sam@manifoldfinance.com>
2022-12-31 07:29:14 -0800

    update

--- scripts/antlr.sh
+++ scripts/antlr.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
 ANTLR_JAR="antlr/antlr4.jar"
 
 if [ ! -e "$ANTLR_JAR" ]; then
-  curl https://www.antlr.org/download/antlr-4.7.2-complete.jar -o "$ANTLR_JAR"
+  curl -sL https://www.antlr.org/download/antlr-4.7.2-complete.jar -o "$ANTLR_JAR"
 fi
 
 java -jar $ANTLR_JAR -Dlanguage=JavaScript antlr/Solidity.g4 -o lib
 
-mv lib/antlr/* src/lib/
+mv lib/antlr/ src/lib/
 
 rmdir lib/antlr
 
